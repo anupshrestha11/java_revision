8bb4237bb15a24a746417a8a4ec64ed52b508aa4
Anup Shrestha <anupshresth9@gmail.com>
2020-12-07 00:24:18 +0545

    code formated

--- Random-program/src/FizzBuzz/FizzBuzz.java
+++ Random-program/src/FizzBuzz/FizzBuzz.java
@@ -3,14 +3,14 @@ package FizzBuzz;
 public class FizzBuzz {
     public static void main(String[] args) {
         StringBuilder output = new StringBuilder();
-        for (int i = 0; i < 100; i++){
-            if (i % 3 == 0){
+        for (int i = 0; i < 100; i++) {
+            if (i % 3 == 0) {
                 output.append("Fizz");
             }
-            if (i%5 == 0){
+            if (i % 5 == 0) {
                 output.append("Buzz");
             }
-            System.out.println(i + " "+ output);
+            System.out.println(i + " " + output);
             output = new StringBuilder();
         }
     }
